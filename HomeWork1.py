df7d65cbfe0ddf2d66dbd4d192ac3647a7d7d827
IgorDenysov <denysov0601@gmail.com>
2023-03-09 13:26:00 +0200

    First Home Task

---
+++ HomeWork1.py
@@ -0,0 +1,3 @@
+name = "Igor"
+day = "Monday"
+print("Good day <name>! <day> is a perfect day to learn some python.")
---
+++ main.py
@@ -0,0 +1,3 @@
+str1 = "#########\n"
+str2 = "#\t\t#\n"
+print(str1 + str2*3 + str1 + "\n" + str2*2 + str1 + str2*2)
